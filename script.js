ced71056bf00de77eba84cb33aa75411ec3fe4c0
Sajid Zaman <zsajid014@gmail.com>
2018-05-21 03:18:49 +0000

    Add another commit

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function sumArray (arr) {
 
 console.log(sumArray([1, 2, 3, 4, 5]));
 console.log(sumArray([19, -2, 30, -45]));
+
+// Task 3
+function checkEmail (emailString) {
+    var emailFormat = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    var match = emailFormat.test(emailString);
+    return match;
+}
+
+console.log(checkEmail("zsajid014@smith.com"));
+console.log(checkEmail("zsajid014smith.info"));
