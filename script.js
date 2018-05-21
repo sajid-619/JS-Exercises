9e93caf1111d07a1f3fce068b8e3c97899739053
Sajid Zaman <zsajid014@gmail.com>
2018-05-21 03:01:52 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 /* JavaScript code here */
-console.log("external JS"); 
+//Task 1
+console.log("external JS");
+
+//Task 2
+function sumArray (arr) {
+    var total = 0;
+    arr.forEach (function (value, index) {
+        total += value;
+        
+    });
+    return total;
+}
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(sumArray([19, -2, 30, -45]));
